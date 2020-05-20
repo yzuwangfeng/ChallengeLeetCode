ecccd8105411adc9cd53cb2f66081ddfca9fc949
levi.wang <levi.wang@ihr360.com>
2020-05-20 21:57:54 +0800

    更新

--- src/com/smlf/test/Solution.java
+++ src/com/smlf/test/Solution.java
@@ -164,11 +164,65 @@ public class Solution {
     }
 
 
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    /**
+     *
+     * 1， 1， 1
+     * 0， 1， 0
+     * 1， 1， 1
+     *
+     * 200. 岛屿数量
+     * @param grid
+     * @return
+     */
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+
     public static void main(String[] args) {
 
         Solution solution = new Solution();
 
-        System.out.println(""+solution.rob(new int[]{ 1, 2, 3, 1 }));
+        int a = 11;int b=12; int c = 11;
+
+        System.out.println(a^=(b^c));
+
+
+//        System.out.println(""+solution.rob(new int[]{ 1, 2, 3, 1 }));
+
+//        char[][] grid = new char[][] {{'1','1','1'},{'0','1','0'},{'1','1','1'}};
+//        System.out.println(solution.numIslands(grid));
 //        List<List<Integer>> lists = solution.combinationSum(new int[]{2,3, 5}, 8);
 //        System.out.println(lists.size());
         // System.out.println(solution.lengthOfLongestSubstring("au"));
